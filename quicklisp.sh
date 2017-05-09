d0176b996ac2a16e290d22e7488cdc8a3b5d0de5
outcastgeek <outcastgeek+git@gmail.com>
2017-05-09 16:34:58 +0000

    Quicklisp Fresh Setup Script for Multiple Implementations

--- quicklisp.sh
+++ quicklisp.sh
@@ -8,14 +8,15 @@ case $1 in
         curl -o ~/quicklisp.lisp https://beta.quicklisp.org/quicklisp.lisp
         curl -o ~/quicklisp.lisp.asc https://beta.quicklisp.org/quicklisp.lisp.asc
         gpg --verify ~/quicklisp.lisp.asc ~/quicklisp.lisp
-        sbcl --non-interactive --load ~/quicklisp.lisp \
+        sbcl --no-sysinit --no-userinit --non-interactive \
+            --load ~/quicklisp.lisp \
             --eval '(quicklisp-quickstart:install)' \
             --eval '(ql:quickload "quicklisp-slime-helper")' \
-            --eval '(ql:add-to-init-file)'
+            --eval '(ql::without-prompting (dolist (imp `(:sbcl :ccl :abcl :ecl)) (ql:add-to-init-file imp)))'
         ;;
 
     clean)
-        rm -rf ~/quicklisp* ~/.sbclrc
+        rm -rf ~/quicklisp* ~/.sbclrc ~/.ccl-init.lisp ~/.abclrc ~/.eclrc
         ;;
 esac
 exit 0
